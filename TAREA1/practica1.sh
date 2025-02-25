4b517d88c65329b1b4c2cab3fa65548b6e4436f7
erwincitoo <erwinerazo17@gmail.com>
2025-02-25 12:41:24 -0400

    se subio la tarea 1.

---
+++ TAREA1/practica1.sh
@@ -0,0 +1,20 @@
+#bin/bash 
+
+## El siguiente comando permite mostrar el directorio actual (PWD):
+pwd
+
+## El siguiente comando accede a la carpeta  TMP y lista su contenido:
+cd /tmp
+ls 
+
+## El siguiente comando nos redirige al directorio personal: 
+cd /home/erwin-mijail-erazo-mamani
+
+## El siguiente  comando crea una carpeta en el directorio en el que nos encontramos (MKDIR): 
+mkdir -p practica_shell
+
+## El siguiente comando accede a la carpeta creada:
+cd /home/erwin-mijail-erazo-mamani/practica_shell
+
+## El siguiente comando muestra el directorio actual (PWD):
+pwd
